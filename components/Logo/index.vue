5d52d68b2c5befc0bbbdd5c3a5a53267edf19b5f
fannychu <tigertop001@gmail.com>
2025-08-21 10:36:30 +0400

    换logo

--- components/Logo/index.vue
+++ components/Logo/index.vue
@@ -4,9 +4,19 @@
     class="logo"
   >
     <img
-      src="./logo.svg"
+      src="/images/comm/logo.svg"
       alt="QuantumBlack"
       class="logo-svg w-50"
+      @error="handleImageError"
     />
   </NuxtLink>
 </template>
+
+<script setup lang="ts">
+  const handleImageError = (event: Event) => {
+    console.error('Logo 加载失败')
+    // 可以设置一个备用图片或显示文字
+    const img = event.target as HTMLImageElement
+    img.style.display = 'none'
+  }
+</script>
